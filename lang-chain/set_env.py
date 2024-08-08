0d9607ac234e38784d0d332c0a38fbdfe8ae7f61
RobinFu <robinfu@189.cn>
2024-08-08 23:06:03 +0800

    [langchain] 增加output_parser

--- lang-chain/set_env.py
+++ lang-chain/set_env.py
@@ -4,7 +4,7 @@ import os
 
 def set_open_ai_key():
     # Set an environment variable
-    os.environ['OPENAI_API_KEY'] = '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'
+    os.environ['OPENAI_API_KEY'] = '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'
 
     # Verify that the environment variable is set
-    print(os.getenv('OPENAI_API_KEY'))
+    print(os.getenv('OPENAI_API_KEY')[-10:])
